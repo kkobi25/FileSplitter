e9fe514bdb40331502fd5b3574c7f225d25ea1dc
kkobi92 <kkamonokobi@gmail.com>
2022-04-28 17:39:14 +0900

    First commit

---
+++ FileSplitter.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FileSpitter
+{
+    partial class FileSplitter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.nud_sizeMB = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_sizeMB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(181, 144);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 13;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // nud_sizeMB
+            // 
+            this.nud_sizeMB.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nud_sizeMB.Location = new System.Drawing.Point(88, 78);
+            this.nud_sizeMB.Name = "nud_sizeMB";
+            this.nud_sizeMB.Size = new System.Drawing.Size(58, 19);
+            this.nud_sizeMB.TabIndex = 14;
+            this.nud_sizeMB.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 12);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Size in MB";
+            // 
+            // FileSplitter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 191);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nud_sizeMB);
+            this.Controls.Add(this.btn_Start);
+            this.Name = "FileSplitter";
+            this.Text = "CSV Splitter";
+            ((System.ComponentModel.ISupportInitialize)(this.nud_sizeMB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.NumericUpDown nud_sizeMB;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileSplitter.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileSpitter
+{
+    public partial class FileSplitter : Form
+    {
+        private List<string> _fileList = null;
+        private string _outputDir = null;
+
+        private const int _headerRow = 1;
+        private double _thresholdinMB = 5;
+        public FileSplitter()
+        {
+            InitializeComponent();
+            _fileList = new List<string>();
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            _fileList.Clear();
+            _thresholdinMB = (double)nud_sizeMB.Value;
+            // Select files to convert
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "CSV file (*.csv)|*.csv;*.CSV|All files (*.*)|*.*";
+                ofd.RestoreDirectory = true;
+                ofd.Title = "Select files to convert.";
+                ofd.Multiselect = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string fn in ofd.FileNames)
+                    {
+                        _fileList.Add(fn);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            // Select directory to save converted files
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                fbd.Description = "Select folder to save converted file";
+                fbd.ShowNewFolderButton = true;
+
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    _outputDir = fbd.SelectedPath;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            List<Task<bool>> tasks = new List<Task<bool>>();
+            foreach (var file in _fileList)
+            {
+                tasks.Add(SplitCSVAysnc(file));
+            }
+            Task.WaitAll(tasks.ToArray());
+            MessageBox.Show("Done!");
+        }
+
+
+
+
+        private async Task<bool> SplitCSVAysnc(string path_OriginalFile)
+        {
+            if (string.IsNullOrEmpty(path_OriginalFile) || !File.Exists(path_OriginalFile))
+                return false;
+
+            StringBuilder sb = new StringBuilder();
+            string headerLine = "";
+            int totalBytes = 0;
+            int fileNumber = 1;
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(path_OriginalFile);
+            string outputFileNameWithoutExtension = Path.Combine(_outputDir, fileNameWithoutExtension);
+            await Task.Run(() =>
+            {
+                using (StreamReader sr = new StreamReader(path_OriginalFile))
+                {
+                    headerLine = sr.ReadLine();
+                    int headerBytes = Encoding.UTF8.GetByteCount(headerLine + "\n");
+                    totalBytes = headerBytes;
+                    while (!sr.EndOfStream)
+                    {
+                        string newLine = sr.ReadLine();
+                        int newLineBytes = Encoding.UTF8.GetByteCount(newLine + "\n");
+                        if (totalBytes + newLineBytes >= _thresholdinMB * 1024.0 * 1024.0)
+                        {
+                            CreateCSV($"{outputFileNameWithoutExtension}_{fileNumber}.csv",
+                                headerLine,
+                                sb);
+                            sb.Clear();
+                            totalBytes = headerBytes;
+                            fileNumber++;
+                        }
+
+                        sb.AppendLine(newLine);
+                        totalBytes += newLineBytes;
+
+                    }
+                }
+                if (sb.Length > 0)
+                {
+                    CreateCSV($"{outputFileNameWithoutExtension}_{fileNumber}.csv",
+                        headerLine,
+                        sb);
+                }
+            }).ConfigureAwait(false);
+
+
+            return true;
+        }
+
+        private bool CreateCSV(string fileName, string headerLine, StringBuilder values)
+        {
+            if (values == null)
+                return false;
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fileName, false, Encoding.UTF8))
+                //using (StreamWriter sw = new StreamWriter(fileName, false, Encoding.GetEncoding("utf-16")))
+                {
+                    sw.WriteLine(headerLine);
+                    sw.Write(values.ToString());
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
